b9b1ff1efd861c8ca1db62139d0902a720c35872
Hatem Knani <hatemknani@gmail.com>
2018-11-18 01:02:02 +0100

    skeleton project in eclipse with JPA and one entity for account. using WF8 runtime and EJB

---
+++ ejbModule/un/bank/entities/account.java
@@ -0,0 +1,48 @@
+package un.bank.entities;
+
+import java.io.Serializable;
+import java.lang.Long;
+import java.util.Date;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: account
+ *
+ */
+@Entity
+public class account implements Serializable {
+	   
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="account_id_seq")
+	@SequenceGenerator(name="account_id_seq", sequenceName="ACCOUNT_ID_SEQ")
+	private Long code;
+	private double balance;
+	private Date dateCreation;
+	private static final long serialVersionUID = 1L;
+
+	public account() {
+		super();
+	}   
+	public Long getCode() {
+		return this.code;
+	}
+
+	public void setCode(Long code) {
+		this.code = code;
+	}   
+	public double getBalance() {
+		return this.balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}   
+	public Date getDateCreation() {
+		return this.dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+   
+}
